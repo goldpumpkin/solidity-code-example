92d0a2531047113cd7072c98eb5fdeba420e572f
gold <goldhuang24@gmail.com>
2024-04-25 00:03:44 +0800

    feat: part ERC721

--- 103/34-1-ERC721.sol
+++ 103/34-1-ERC721.sol
@@ -33,11 +33,61 @@ contract ERC721 is IERC721, IERC721Metadata{
                 interfaceId == type(IERC721Metadata).interfaceId;
     }
 
+    // 实现 IERC721 的 balanceOf，利用 _balances 变量查询 owner 地址的 balance。
     function balanceOf(address owner) external view override returns(uint) {
         require(owner != address(0), "owner = zero address");
         return _balances[owner];
     }
 
+    // 实现 IERC721 的 ownerOf，利用 _owners 变量查询 tokenId 的 owner。
+    function ownerOf(uint tokenId) public view override returns(address owner) {
+        owner = _owners[tokenId];
+        require(owner != address(0), "token doesn't exist");
+    }
 
+    // 实现 IERC721 的 isApprovedForAll，利用 _operatorApprovals 变量查询 owner 地址是否将所持 NFT 批量授权给了 operator 地址。
+    function isApprovedForAll(address owner, address operator) external view override returns(bool) {
+        return _operatorApprovals[owner][operator];
+    }
 
+    // 实现 IERC721 的 setApprovalForAll，将持有代币全部授权给 operator 地址。调用 _setApprovalForAll 函数。
+    function serApprovedForAll(address operator, bool approved) external override {
+        _operatorApprovals[msg.sender][operator] = approved;
+
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    // 实现 IERC721 的 getApproved，利用 _tokenApprovals 变量查询 tokenId 的授权地址。
+    function getApproved(uint tokenId) external view override returns(address) {
+        require(_owners[tokenId] != address(0), "token doesn't exist");
+        return _tokenApprovals[tokenId];
+    }
+
+    function _approve(address owner, address to, uint tokenId) private {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    function approve(address to, uint tokenId) external override {
+        address owner = _owners[tokenId];
+        require(owner == msg.sender || _operatorApprovals[owner][msg.sender], "not owner nor approved for all");
+        _approve(owner, to, tokenId);
+    }
+
+    function _isApprovedOrOwner(address owner, address spender, uint tokenId) private view returns(bool) {
+        return (spender == owner || _operatorApprovals[owner][spender] || _tokenApprovals[tokenId] == spender);
+    }
+
+    function _transfer(address owner, address from, address to, uint tokenId) private {
+        require(from == owner,  "not owner");
+        require(to != address(0), "transfer to the zero address");
+
+        _approve(owner, address(0), tokenId);
+
+         _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
 }
